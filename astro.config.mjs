536358af7d158a1c1d612c355a5532da3a51bf3f
Marcus V. Ximenes <marcus.ximenes@gupy.com.br>
2025-07-01 14:52:19 -0300

    Initial setup with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  integrations: [
+    tailwind(),
+    react()
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
-
     imageService: "cloudflare"
   })
 });
